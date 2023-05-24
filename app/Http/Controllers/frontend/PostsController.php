679d4b12a295d3dadd8b1f6c6439ec729c8c3a6f
edgar-github <edgar93serobyan@gmail.com>
2023-05-24 13:13:03 +0400

    create pedia posts functional

--- app/Http/Controllers/frontend/PostsController.php
+++ app/Http/Controllers/frontend/PostsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use App\Models\PostCategory;
-use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
@@ -14,24 +13,18 @@ class PostsController extends Controller
      */
     public function articles(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-//        dd(self::getPostCategories());
         $posts = Post::all();
         $postCategories = self::getPostCategories();
         return view('article/articles', compact('posts'), compact('postCategories'));
     }
 
-    public static function getPostCategories(): \Illuminate\Database\Eloquent\Collection|array
-    {
-       return PostCategory::with('post')->get();
-//        dd($categories);
-    }
 
     /**
      * Display the specified resource.
      */
     public function view(Post $post, $slug): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $otherPosts = $this->otherPosts($slug);
+        $otherPosts = self::otherPosts($slug);
         $post = $post::where('slug', $slug)
             ->firstOrFail();
 
@@ -39,15 +32,44 @@ class PostsController extends Controller
     }
 
     /**
+     * @param $slug
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+     */
+    public function medias($slug): \Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        $postCategoryID = self::getPostCategoryId($slug);
+        $mediaPosts = Post::where('post_category_id', $postCategoryID)->get();
+
+        return view('article/medias', compact('mediaPosts'));
+    }
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|array
+     */
+    public static function getPostCategories(): \Illuminate\Database\Eloquent\Collection|array
+    {
+        return PostCategory::with('post')->get();
+    }
+
+    /**
+     * @param $slug
+     * @return \Illuminate\Database\Eloquent\Collection|array
+     */
+    public static function getPostCategoryId($slug): \Illuminate\Database\Eloquent\Collection|array
+    {
+        return  PostCategory::where('slug', $slug)->firstOrFail()->id;
+    }
+
+    /**
      * @param $id
      * @return mixed
      */
-
-    public function otherPosts($id): mixed
+    public static function otherPosts($id): mixed
     {
         return Post::where('slug', '!=', $id)
             ->inRandomOrder()
-            ->limit(4)
+            ->limit(Post::OTHERS_POSTS_LIMIT)
             ->get();
     }
 }
---
+++ resources/views/article/medias.blade.php
@@ -0,0 +1,77 @@
+@extends('layout.layout')
+
+@section('content')
+    <main class="news-articles">
+        <div class="section-tab">
+            <div class="wrapper content">
+                <div class="tabs_wrap">
+                    <ul>
+                        <li data-tabs="article" class="article-li">Հոդվածներ</li>
+                        <li data-tabs="media" class="media-li">Մեդիա</li>
+                    </ul>
+                </div>
+                <div class="article-tab">
+                    <div class="tab-items-article">
+                        <div class="item_wrap articles-wrapper open">
+                            <div class="articles-list">
+                                @foreach($posts as $post)
+                                    <div class="articles-list-item">
+                                        <div class="articles-list-item-img">
+                                            <a href="{{ LaravelLocalization::localizeUrl('/article/' . $post['slug']) }}">
+                                                <img src="{{ URL::to('storage/' . $post['image']) }}" alt="">
+                                            </a>
+                                        </div>
+                                        <p>{{ $post['title_' . app()->getLocale()] }}</p>
+                                        <span>{{ $post['created_at'] }}</span>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+
+                        <div class="item_wrap media-wrapper">
+                            <div class="item">
+                                <div class="item_left">
+                                    <div class="data section-media-info">
+                                        <div class="choose-media-section">
+                                            <h3>Ընտրել մեդիան</h3>
+                                            <div class="media-scrol-bar scrollbar" id="style-4">
+
+                                                <div class="scroll-bar-all scrol-bar-item">
+                                                    <p>Բոլորը</p>
+                                                </div>
+
+                                                @foreach($postCategories as $postCategory)
+                                                    <div class="scrol-bar-item">
+                                                        <div class="scrol-bar-item-img">
+                                                            <img src="{{ URL::to($postCategory['image']) }}" alt="">
+                                                        </div>
+                                                        <a href="{{ LaravelLocalization::localizeUrl('/media/' . $postCategory['id']) }}">
+                                                            <p>{{ $postCategory['title_' . app()->getLocale()] }}</p>
+                                                        </a>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                        <div class="media-list-section">
+                                            @foreach($posts as $post)
+                                                <div class="media-list-item">
+                                                    <div class="media-list-item-img">
+                                                        <a href="{{ LaravelLocalization::localizeUrl('/article/' . $post['slug']) }}">
+                                                            <img src="{{ URL::to('storage/' . $post['image']) }}" alt="">
+                                                        </a>
+                                                    </div>
+                                                    <p>{{ $post['title_' . app()->getLocale()] }}</p>
+                                                    <span>{{ $post['created_at'] }}</span>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection

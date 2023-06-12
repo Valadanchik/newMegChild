7d222c7cfeff6c2122f0e6cf87bec5d94aaa4f2b
ElenGhulyan <elen.ghulyan2@gmail.com>
2023-06-12 13:23:26 +0400

    finish not found section

--- resources/views/book/books.blade.php
+++ resources/views/book/books.blade.php
@@ -13,7 +13,7 @@
                     @endforeach
                 </ul>
             </div>
-            <div class="books-info-item">
+            {{--<div class="books-info-item">
                 @foreach($books as $index => $book)
                     <div class="book-item">
                         <div class="book-item-images">
@@ -42,7 +42,49 @@
                         </div>
                     </div>
                 @endforeach
-            </div>
+            </div>--}}
+            @if(count($books))
+                <div class="books-info-item">
+                    @foreach($books as $index => $book)
+                        <div class="book-item">
+                            <div class="book-item-images">
+                                <div class="book-item-img-logo">
+                                    <img src="{{ URL::to('images/reade-more-img-logo-green.png') }}" alt="">
+                                </div>
+                                <div class="book-item-img-book">
+                                    <a href="{{ LaravelLocalization::localizeUrl('/book/' . $book['slug']) }}">
+                                        <img width="270px" src="{{ URL::to('storage/' . $book['main_image']) }}" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <h3>{{ $book['title_' . app()->getLocale()]  }}</h3>
+                            <p>
+                                @foreach($book->authors as $key => $author)
+                                    {{ $author['name_' . app()->getLocale()] }} {{ $key < count($book->authors) - 1 ? ',' : '' }}
+                                @endforeach
+                            </p>
+                            <div class="book-price">
+                                <p class="book-pr">{{ $book['price']  }} ֏ </p>
+                            </div>
+                            <div class="book-btn">
+                                <a href="{{ LaravelLocalization::localizeUrl('/book/' . $book['slug']) }}">
+                                    Գնել
+                                </a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+
+                <div class="not-found">
+                    <div class="not-found-img">
+                        <img src="{{ URL::to('/') }}/images/girl.png" alt="girl images">
+                    </div>
+                    <p>Այս բաժնում դեռևս գրքեր չկան</p>
+                </div>
+            @endif
+
+
         </section>
     </main>
 @endsection

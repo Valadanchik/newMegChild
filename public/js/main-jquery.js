73ccddab5a9afab8e63a81440afd7762f3203df1
edgar-github <edgar93serobyan@gmail.com>
2023-06-12 13:17:53 +0400

    finish search filter

--- public/js/main-jquery.js
+++ public/js/main-jquery.js
@@ -9,3 +9,81 @@ $('.single-item').slick({
         '<path d="M1 13L7 7L1 1" stroke="#8F52A1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
         '</svg>'),
 });
+
+/*/////////SLIDER//////////////*/
+
+/*SEARCH FUNCTIONALITY*/
+$(document).ready(function () {
+    $('#search-input').on('keyup', function (event) {
+        if (event.target.value.length > 2) {
+            $('.loader-container').show();
+
+            $.ajax({
+                url: $('#search-route-name').val(),
+                type: 'POST',
+                dataType: 'json',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: JSON.stringify({
+                    _token: $('meta[name="csrf-token"]').attr('content'),
+                    search: event.target.value
+                }),
+                success: function (data) {
+                    htmlFilter(data);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.error(errorThrown);
+                },
+                complete: function () {
+                    $('.loader-container').hide();
+                }
+            });
+        } else {
+            $("#booksContainer").html('');
+        }
+    });
+});
+
+function htmlFilter(data) {
+    let bookUrl = $("#book-url").val();
+    let bookImageUrl = $("#book-image-url").val();
+    let locale = $("#locale").val();
+    let bookItemLogo = $("#book-item-logo").val();
+    let booksInfoItem = $('<div class="books-info-item"></div>');
+
+    data.books.forEach((book, index) => {
+        let searchData = '';
+        searchData +=
+            `<div class="book-item">
+                <div class="book-item-images">
+                    <div class="book-item-img-logo">
+                        <img src="${bookItemLogo}" alt="">
+                    </div>
+                    <div class="book-item-img-book">
+                        <a href="${bookUrl + '/' + book.slug}">
+                            <img width="270px" src="${bookImageUrl + '/' + book.main_image}" alt="">
+                        </a>
+                    </div>
+                </div>
+                <h3>
+                    ${book['title_' + locale]}
+                </h3>
+                <p>
+                    ${book.authors.map((author) => author['name_' + locale])}
+                </p>
+                <div class="book-price">
+                    <p class="book-pr">${book.price} ֏</p>
+                </div>
+                <div class="book-btn">
+                    <a href="${bookUrl + '/' + book.slug}">
+                        Գնել
+                    </a>
+                </div>
+            </div>`;
+        booksInfoItem.append(searchData);
+    })
+    $("#booksContainer").html(booksInfoItem);
+}
+/*SEARCH FUNCTIONALITY*/
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -5,51 +5,27 @@
             <img src="{{ URL::to('/images/svg/close-popup.svg') }}" alt="close">
         </div>
 
-        <form class="search-box">
-            <button style="background: none; border: none; padding: 0"><span>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                             <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
-                                   stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                             <path d="M20.9984 21L16.6484 16.65" stroke="black" stroke-width="2" stroke-linecap="round"
-                                   stroke-linejoin="round"/>
-                              </svg>
-                        </span>
-            </button>
-            <input type="search" name="search" placeholder="Որոնել" id="search-input" value="">
-            <input type="hidden" name="search" id="search-route-name" value="{{ route("books.search") }}">
-        </form>
-
-        <div id="booksContainer"></div>
-
+        <div>
+            <form class="search-box">
+                <button style="background: none; border: none; padding: 0">
+                    <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+                                stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M20.9984 21L16.6484 16.65" stroke="black" stroke-width="2" stroke-linecap="round"
+                                  stroke-linejoin="round"/>
+                        </svg>
+                    </span>
+                </button>
+                <input type="search" name="search" placeholder="Որոնել" id="search-input" value="">
+                <input type="hidden" id="search-route-name" value="{{ route("books.search") }}">
+                <input type="hidden" id="book-url" value="{{ LaravelLocalization::localizeUrl('/book/') }}">
+                <input type="hidden" id="book-image-url" value="{{ URL::to('storage/') }}">
+                <input type="hidden" id="locale" value="{{ app()->getLocale() }}">
+                <input type="hidden" id="book-item-logo" value="{{ URL::to('images/reade-more-img-logo-green.png') }}">
+            </form>
+            <div id="booksContainer"></div>
+        </div>
     </div>
-
-
-
 </div>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script>
-    $('#search-input').on('keyup', function(event) {
-        $.ajax({
-            url: $('#search-route-name').val(),
-            type: 'POST',
-            dataType: 'json',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-            },
-            data: JSON.stringify({ _token: $('meta[name="csrf-token"]').attr('content'), search: event.target.value }),
-            success: function(data) {
-
-                console.log(data);
-
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                console.error(errorThrown);
-            },
-            complete: function() {
-                // $('.loader-container').hide();
-            }
-        });
-    });
-</script>

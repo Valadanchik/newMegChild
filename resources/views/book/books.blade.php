23fe874cebe9bb26cdabe7a7b8e872d59dc23455
ElenGhulyan <elen.ghulyan2@gmail.com>
2023-06-12 13:29:19 +0400

    deleted comments

--- resources/views/book/books.blade.php
+++ resources/views/book/books.blade.php
@@ -5,7 +5,8 @@
         <section class="books content">
             <div class="filtering-book-page">
                 <ul class="filtering-book-page-list">
-                    <li class="{{ $slug === null ? 'is-checked' : '' }}"><a href="{{ LaravelLocalization::localizeUrl('/books') }}">Ամբողջը</a></li>
+                    <li class="{{ $slug === null ? 'is-checked' : '' }}"><a
+                            href="{{ LaravelLocalization::localizeUrl('/books') }}">Ամբողջը</a></li>
                     @foreach($categories as $category)
                         <li class="{{ $slug == $category['name_en'] ? 'is-checked' : '' }}">
                             <a href="{{ LaravelLocalization::localizeUrl('/category/' . $category['name_en']) }}">{{ $category['name_' . app()->getLocale()] }}</a>
@@ -13,36 +14,6 @@
                     @endforeach
                 </ul>
             </div>
-            {{--<div class="books-info-item">
-                @foreach($books as $index => $book)
-                    <div class="book-item">
-                        <div class="book-item-images">
-                            <div class="book-item-img-logo">
-                                <img src="{{ URL::to('images/reade-more-img-logo-green.png') }}" alt="">
-                            </div>
-                            <div class="book-item-img-book">
-                                <a href="{{ LaravelLocalization::localizeUrl('/book/' . $book['slug']) }}">
-                                    <img width="270px" src="{{ URL::to('storage/' . $book['main_image']) }}" alt="">
-                                </a>
-                            </div>
-                        </div>
-                        <h3>{{ $book['title_' . app()->getLocale()]  }}</h3>
-                        <p>
-                            @foreach($book->authors as $key => $author)
-                                {{ $author['name_' . app()->getLocale()] }} {{ $key < count($book->authors) - 1 ? ',' : '' }}
-                            @endforeach
-                        </p>
-                        <div class="book-price">
-                            <p class="book-pr">{{ $book['price']  }} ֏ </p>
-                        </div>
-                        <div class="book-btn">
-                            <a href="{{ LaravelLocalization::localizeUrl('/book/' . $book['slug']) }}">
-                                Գնել
-                            </a>
-                        </div>
-                    </div>
-                @endforeach
-            </div>--}}
             @if(count($books))
                 <div class="books-info-item">
                     @foreach($books as $index => $book)
@@ -75,7 +46,6 @@
                     @endforeach
                 </div>
             @else
-
                 <div class="not-found">
                     <div class="not-found-img">
                         <img src="{{ URL::to('/') }}/images/girl.png" alt="girl images">
